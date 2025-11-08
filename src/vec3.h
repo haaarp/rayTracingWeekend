6b922345f113d48dd656533028ee285b80cda6f9
Kasimir Rothbauer <kasimir@brumas.com>
2025-11-08 22:52:59 +0200

    Beginning of vec3 header file

--- src/vec3.h
+++ src/vec3.h
@@ -10,6 +10,22 @@ class vec3 {
 	double e[3];
 
 	vec3() : e{0,0,0} {}
+	vec3(double e0, double e1, double e2) : e{e0,e1,e2} {}
+
+
+	double  x() const {return e[0]; }
+	double  y() const {return e[1]; }
+	double  z() const {return e[2]; }
+
+
+	vec3 operator-() const { return vec3(-e[0], -e[1],  -e[2]);} 
+	double operator[](int i) const { return e[i]; }
+	double& operator[](int i) { return e[i]; }
+
+	vec3& operator+=(const vec3& v) {
+	    return *this;
+	}
+
 };
 
 #endif
