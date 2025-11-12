5c1d0342e0b981d9547d25a45f0087720acc4d8e
Kasimir Rothbauer <kasimir@brumas.com>
2025-11-12 18:32:24 +0200

    Add random double vector generator

--- src/vec3.h
+++ src/vec3.h
@@ -1,6 +1,7 @@
 #ifndef VEC3_H
 #define VEC3_H
 
+#include "rtweekend.h"
 class vec3 {
 
     public:
@@ -46,6 +47,18 @@ class vec3 {
 	double length_squared() const {
         return e[0]*e[0] + e[1]*e[1] + e[2]*e[2];
     }
+
+
+    static vec3 random() {
+        return vec3(random_double(),random_double(),random_double());
+
+    }
+
+    static vec3 random(double min, double max) {
+        return vec3(random_double(min,max), random_double(min,max), random_double(min,max));
+    }
+
+        
 
 };
 
