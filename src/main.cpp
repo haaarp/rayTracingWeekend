ab54df221478dae04f0dc1f32c0aea81acfe949f
kasimir <kasimir@brumas.com>
2026-01-20 19:51:16 +0100

    add the bvh nodes to world for actual speedup

--- src/main.cpp
+++ src/main.cpp
@@ -53,7 +53,7 @@ int main() {
 
     auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
     world.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
-
+    world = hittable_list(make_shared<bvh_node>(world));
     camera cam;
 
     cam.aspect_ratio      = 16.0 / 9.0;
